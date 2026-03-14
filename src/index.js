d46e39df722e6ff36e6f5da0614d03857203c842
Rizwansh-dev <255385401+rizwanshaukat-dev@users.noreply.github.com>
2026-03-14 22:41:52 +0500

    add tab switching logic for menu and home page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-alert("Hello World")
+import home from './home.js';
+import menu from './menu.js';
+import './styles/global.css';
+
+home();
+
+function reset() {
+    const contentDiv = document.getElementById("content");
+    if (contentDiv) contentDiv.innerHTML = "";
+}
+
+function loadPage(page) {
+    reset();
+    page();
+}
+
+const homeBtn = document.getElementById("home-btn");
+const menuBtn = document.getElementById("menu-btn");
+const aboutBtn = document.getElementById("about-btn");
+
+homeBtn.addEventListener("click", () => loadPage(home));
+menuBtn.addEventListener("click", () => loadPage(menu));
