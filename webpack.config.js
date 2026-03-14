9c4bb1a6955d0aa99842eaca544db9a4dadc1f42
Rizwansh-dev <255385401+rizwanshaukat-dev@users.noreply.github.com>
2026-03-14 23:26:43 +0500

    remove redundant modules in webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -32,10 +32,6 @@ export default {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.html$/i,
-        use: ["html-loader"],
-      },
-      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
